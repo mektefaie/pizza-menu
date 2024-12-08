d1f2e49220790c1d9655fc01a8b4df0e667648bd
mahmoud_e <54193376+mektefaie@users.noreply.github.com>
2024-12-07 20:49:20 -0800

    add order component

--- src/index.js
+++ src/index.js
@@ -58,9 +58,11 @@ function App() {
 }
 
 function Header() {
+  const style = {}
+
   return (
     <header className='header'>
-      <h1>Fast React Pizza Co.</h1>
+      <h1 style={style}>Fast React Pizza Co.</h1>
     </header>
   )
 }
@@ -101,16 +103,25 @@ function Footer() {
   return (
     <footer className='footer'>
       {isOpen ? (
-        <div className='order'>
-          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
-          <button className='btn'>Order</button>
-        </div>
+        <Order closeHour={closeHour} openHour={openHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00.
         </p>
       )}
     </footer>
+  )
+}
+
+function Order({ closeHour, openHour }) {
+  return (
+    <div className='order'>
+      <p>
+        We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
+        online.
+      </p>
+      <button className='btn'>Order</button>
+    </div>
   )
 }
 
